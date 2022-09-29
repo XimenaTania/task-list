d3538f2ad5d0aa6b1fc18c99d5135d99b7561def
XimenaTania <xime.tania17@gmail.com>
2022-09-29 01:14:41 -0400

    subiendoCmabio

---
+++ src/componentes/TodoItem.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+function TodoItem({ todo, onComplete, onDeleteItem }) {
+
+    const getSyle = () => {
+        return {
+            textDecoration: todo.completed ? 'line-through' : 'none',
+            margin: '20px',
+            border: '1px solid #ffffff',
+            backgroundColor: '#CCF7E3'
+        }
+    }
+    return ( <
+        div style = { getSyle() } >
+        <
+        input type = 'checkbox'
+        checked = { todo.completed }
+        onChange = {
+            () => onComplete(todo.id) }
+        />
+
+        { todo.task } <
+        button className = 'add-btn'
+        onClick = {
+            () => onDeleteItem(todo.id) } > X < /button> <
+        /div>
+    )
+}
+export default TodoItem;
